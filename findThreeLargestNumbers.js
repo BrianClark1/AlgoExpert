ab710526876183947c3b6f734e6dbada4f6bd746
Brian Clark <briansclark1@gmail.com>
2022-06-15 08:44:23 -0400

    findThreeLargest complete

---
+++ findThreeLargestNumbers.js
@@ -0,0 +1,52 @@
+//Can obviously be done with 3 for loops to find each number
+//However I have suspicions that It can be done in a single loop
+//Possibly in constant space
+//Also a suspicion that It can be done without sorting the output array as well
+
+//My attempt
+//I will create an output array
+//I will create 3 variables to keep track of the 3 largest
+//I will loop thorugh the array and make comparisons with indexes
+//and largest variables to extrapolate the 3 largest
+//Then sort the output array.
+
+//O(nlogn) time and O(1) space, always 3 numbers
+//I presume optimal is O(n) O(1) we will see though
+function findThreeLargestNumbers(array) {
+  let output = [];
+
+  //Find largest
+  let max = -Infinity;
+  let maxIdx;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+      maxIdx = i;
+    }
+  }
+  output.push(max);
+  array.splice(maxIdx, 1);
+
+  let secondLargest = -Infinity;
+  let secondIdx;
+  for (let k = 0; k < array.length; k++) {
+    if (array[k] > secondLargest) {
+      secondLargest = array[k];
+      secondIdx = k;
+    }
+  }
+  output.push(secondLargest);
+  array.splice(secondIdx, 1);
+
+  let thirdLargest = -Infinity;
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] > thirdLargest) {
+      thirdLargest = array[j];
+      thirdIdx = j;
+    }
+  }
+
+  output.push(thirdLargest);
+
+  return output.sort((a, b) => a - b);
+}
