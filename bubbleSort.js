8e0bc8d1cc8acc3325f8bda491b89518549427ce
Brian Clark <briansclark1@gmail.com>
2022-06-29 10:50:25 -0400

    optimized bubbleSort Solution

--- bubbleSort.js
+++ bubbleSort.js
@@ -4,17 +4,21 @@
 //meaning the sorting process is complete and we can return the array
 //its going to take multiple passes through so once idx reaches the end we will need to reset
 
+
+//(On^2) time | O(1) space 
 function bubbleSort(array) {
   let trigger = true;
+  let counter = 0;
   while (trigger) {
     trigger = false;
-    for (let i = 0; i <= array.length - 2; i++) {
+    for (let i = 0; i <= array.length - 2 - counter; i++) {
       if (array[i] > array[i + 1]) {
         //swap
         [array[i], array[i + 1]] = [array[i + 1], array[i]];
         trigger = true;
       }
     }
+    counter++;
   }
   return array;
 }
